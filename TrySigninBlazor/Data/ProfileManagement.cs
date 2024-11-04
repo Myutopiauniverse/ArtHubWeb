cde52b89efcd9d7660f44bab3282a1ae2d05512f
Myutopiauniverse <s224040405@mandela.ac.za>
2024-11-04 05:19:04 +0200

    This is the first push

--- TrySigninBlazor/Data/ProfileManagement.cs
+++ TrySigninBlazor/Data/ProfileManagement.cs
@@ -1,7 +1,17 @@
-﻿namespace TrySigninBlazor.Data
+﻿using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+
+namespace TrySigninBlazor.Data
 {
     public class ProfileManagement
     {
-
+        public int Id { get; set; }
+        public int Username { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Biography { get; set; }
+        public string MissionStatement { get; set; }
+        public string Document {  get; set; }
+        public byte[] Profile {  get; set; }
     }
 }
--- TrySigninBlazor/Program.cs
+++ TrySigninBlazor/Program.cs
@@ -5,6 +5,7 @@ using TrySigninBlazor.Components;
 using TrySigninBlazor.Components.Account;
 using TrySigninBlazor.Data;
 using Microsoft.Extensions.DependencyInjection;
+using TrySigninBlazor.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContextFactory<TrySigninBlazorContext>(options =>
@@ -22,10 +23,10 @@ builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
 builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
+{
+    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+})
     .AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -59,10 +60,14 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseStaticFiles();
+app.UseRouting();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.UseEndpoints(endpoints =>
+{
+    _ = endpoints.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+    _ = endpoints.MapHub<BlazorChatSampleHub>(BlazorChatSampleHub.HubUrl);
+});
 
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
